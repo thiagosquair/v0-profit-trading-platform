0a96189fdf7652e0628541af53f72d2c61304e10
thiagosquair <squair@live.com>
2025-06-19 16:25:49 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,8 @@
+// next.config.mjs
+import createNextIntlPlugin from "next-intl/plugin";
+
+const withNextIntl = createNextIntlPlugin();
+
 /** @type {import("next").NextConfig} */
 const nextConfig = {
   eslint: {
@@ -10,6 +15,6 @@ const nextConfig = {
     domains: ["placeholder.svg"],
     unoptimized: true,
   },
-}
+};
 
-export default nextConfig
+export default withNextIntl(nextConfig);
