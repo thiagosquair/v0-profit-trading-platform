42860ee4c4a31edbe451b893d0f6d72273da87ac
thiagosquair <squair@live.com>
2025-06-19 15:46:44 +0100

    Create layout.tsx

---
+++ app/[locale]/layout.tsx
@@ -0,0 +1,39 @@
+import {NextIntlClientProvider} from 'next-intl';
+import {getMessages} from 'next-intl/server';
+import {notFound} from 'next/navigation';
+import {Inter} from 'next/font/google';
+import '../globals.css';
+
+const inter = Inter({subsets: ['latin']});
+
+const locales = ['en', 'pt', 'es', 'fr'];
+
+export function generateStaticParams() {
+  return locales.map((locale) => ({locale}));
+}
+
+export default async function LocaleLayout({
+  children,
+  params: {locale}
+}: {
+  children: React.ReactNode;
+  params: {locale: string};
+}) {
+  // Validate that the incoming `locale` parameter is valid
+  if (!locales.includes(locale as any)) notFound();
+
+  // Providing all messages to the client
+  // side is the easiest way to get started
+  const messages = await getMessages();
+
+  return (
+    <html lang={locale}>
+      <body className={inter.className}>
+        <NextIntlClientProvider messages={messages}>
+          {children}
+        </NextIntlClientProvider>
+      </body>
+    </html>
+  );
+}
+
