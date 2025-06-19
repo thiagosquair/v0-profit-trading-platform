c9e768b8125a1d0ef33cadeabd4f2fb78dc438b6
thiagosquair <squair@live.com>
2025-06-19 16:10:27 +0100

    Update page.tsx

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,6 +1,6 @@
 import type React from "react"
 import type { Metadata } from "next"
-import "./globals.css"
+import "../globals.css"
 import { LanguageProvider } from "@/hooks/use-language"
 import { ThemeProvider } from "@/components/theme-provider"
 import { Inter } from "next/font/google"
