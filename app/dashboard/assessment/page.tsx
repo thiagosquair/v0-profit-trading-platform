293af81dfad3734e8c719487ff16b5c7cfd20e08
thiagosquair <squair@live.com>
2025-07-10 11:33:23 +0100

    Update page.tsx

--- app/dashboard/assessment/page.tsx
+++ app/dashboard/assessment/page.tsx
@@ -1,9 +1,5 @@
 import { TraderAssessment } from '@/components/assessment/trader-assessment'
 
 export default function AssessmentPage() {
-  return (
-    <div className="min-h-screen bg-gray-50">
-      <TraderAssessment />
-    </div>
-  )
+  return <TraderAssessment />
 }
